42a2fb5c44ec5979ada679dbdbe3099a2ba8b53e
Martin Baláž <martin.balaz@trojsten.sk>
2026-07-20 09:30:20 +0000

    Better import script

--- db_import.sh
+++ db_import.sh
@@ -8,7 +8,7 @@ SQL=treeline.sql
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 echo "Fetching $SQL from amos..."
-scp amos:~/$SQL ~/$SQL
+scp amos:~/treeline/$SQL ./$SQL
 
 echo "Restarting PostgreSQL..."
 sudo systemctl restart postgresql
